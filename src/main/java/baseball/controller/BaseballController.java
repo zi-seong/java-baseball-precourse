5c7e5d6fed11189a282b9859455429643bcaadca
zi-seong <jiseong4577@gmail.com>
2022-10-03 16:11:40 +0900

    feat : BaseballController 추가

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,40 @@
+package baseball.controller;
+
+import baseball.domain.*;
+import baseball.enums.Messages;
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseballController {
+
+    private final Player player = new Player();
+    private final Computer computer = new Computer();
+
+    public void start() {
+        BaseballResult baseballResult;
+        computer.pickNumbers();
+
+        do {
+            System.out.print(Messages.START.getText());
+            player.setInputNumbers(Console.readLine().trim());
+
+            BaseballSystem baseballSystem = new BaseballSystem(player, computer);
+            baseballResult = baseballSystem.play();
+
+            System.out.println(baseballResult.convert(baseballResult));
+        } while (baseballResult.getStrikeCount() != 3);
+
+        gameOver();
+    }
+
+
+    private void gameOver() {
+        System.out.println(Messages.END.getText());
+        System.out.println(Messages.OPTION.getText());
+
+        String input = Console.readLine().trim();
+
+        if (input.equals("1")) {
+            start();
+        }
+    }
+}
