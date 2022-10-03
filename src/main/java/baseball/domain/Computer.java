d3c5182975c47298804c73ab836713f3b460c2c6
unknown <jiseong4577@gmail.com>
2022-10-03 13:42:46 +0900

    feat : 컴퓨터 숫자 생성
    Computer 도메인 추가

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,2 +1,20 @@
-package baseball.domain;public class Computer {
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    private final int MAX_NUMBER = 9;
+    private final int MIN_NUMBER = 1;
+
+    private final List<Integer> numbers = new ArrayList<>();
+
+    public void pickNumbers() {
+        numbers.clear();
+        numbers.add(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
+    }
+
 }
